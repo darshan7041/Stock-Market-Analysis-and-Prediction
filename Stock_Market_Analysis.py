d91857605e9a89568f38b958d233f5c4cca2d68d
Darshan Solanki <156898968+darshan7041@users.noreply.github.com>
2025-01-23 10:18:08 +0530

    Update Stock_Market_Analysis.py

--- Stock_Market_Analysis.py
+++ Stock_Market_Analysis.py
@@ -8,7 +8,7 @@ import yfinance as yf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
 
-# Step 1: Data Collection
+# Step 1: Data Collect
 ticker = "MSFT"
 data = yf.download(ticker, start="2010-01-01", end="2023-01-01")
 if data.empty or 'Close' not in data:
@@ -20,11 +20,11 @@ print(data.head())
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(data)
 
-# Prepare training and testing data
+# Prepare train and test data
 train_size = int(len(scaled_data) * 0.8)
 train_data, test_data = scaled_data[:train_size], scaled_data[train_size:]
 
-# Create sequences for LSTM
+# sequences for LSTM
 def create_sequences(data, seq_length):
     x, y = [], []
     for i in range(seq_length, len(data)):
@@ -37,12 +37,12 @@ x_train, y_train = create_sequences(train_data, seq_length)
 x_test, y_test = create_sequences(test_data, seq_length)
 
 
-# Reshape data for LSTM
+# Reshape
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
 
-# Step 3: Build LSTM Model
+# Step 3: LSTM Model
 model = Sequential([
     LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)),
     Dropout(0.2),
